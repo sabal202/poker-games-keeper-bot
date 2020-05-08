b85e963831226815b5b2c9e3744a1aac66f8ceeb
Sergey Sabalevsky <sabal2000@mail.ru>
2020-05-08 17:44:46 +0300

    fixed order

--- bot.py
+++ bot.py
@@ -8,14 +8,6 @@ bot = telebot.TeleBot(TOKEN)
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 @bot.message_handler(commands=['poker_start'])
 def handle_poker_start(message):
@@ -40,5 +32,15 @@ def handle_poker_event(message):
 @bot.message_handler(commands=['poker_undo'])
 def handle_poker_undo(message):
 	bot.reply_to(message, message.text)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
 
 bot.polling(none_stop=True, interval=0, timeout=20)
