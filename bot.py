4ce7c258148c0070753f9ce257aef1e7264cdc47
Sergey Sabalevsky <sabal2000@mail.ru>
2020-05-08 17:41:23 +0300

    reply all

--- bot.py
+++ bot.py
@@ -19,26 +19,26 @@ def get_text_messages(message):
 
 @bot.message_handler(commands=['poker_start'])
 def handle_poker_start(message):
-	print(message)
+	bot.reply_to(message, message.text)
 
 @bot.message_handler(commands=['poker_end'])
 def handle_poker_end(message):
-	print(message)
+	bot.reply_to(message, message.text)
 
 @bot.message_handler(commands=['poker_results'])
 def handle_poker_results(message):
-	print(message)
+	bot.reply_to(message, message.text)
 
 @bot.message_handler(commands=['poker_note'])
 def handle_poker_note(message):
-	print(message)
+	bot.reply_to(message, message.text)
 
 @bot.message_handler(commands=['poker_event'])
 def handle_poker_event(message):
-	print(message)
+	bot.reply_to(message, message.text)
 
 @bot.message_handler(commands=['poker_undo'])
 def handle_poker_undo(message):
-	print(message)
+	bot.reply_to(message, message.text)
 
 bot.polling(none_stop=True, interval=0, timeout=20)
